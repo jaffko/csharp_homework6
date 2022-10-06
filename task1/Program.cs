c39c6cd89f61a1957e2256b2f75ad52af0693842
Ildar <steel14@yandex.ru>
2022-10-07 00:14:08 +0500

    added task1

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int positiveNums(int[] array)
+{
+    int result = 0;
+    foreach (var item in array)
+    {
+        if (item > 0) result++;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число чисел");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+Console.WriteLine("введите числа");
+for (int i = 0; i < M; i++) array[i] = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Количество положительных чисел {positiveNums(array)}");
